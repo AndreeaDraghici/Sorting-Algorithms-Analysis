aa3454b6e665a2256b42fa22fd2adb01ca58cde2
AndreeaDraghici <draghici108@gmail.com>
2024-02-19 22:24:35 +0200

    first commit

--- Program.cs
+++ Program.cs
@@ -3,14 +3,13 @@ using MEPSortingAlgorithms;
 using MEPSortingAlgorithms.seqvential;
 
 
-RandomGenerator generator = new RandomGenerator();
+//RandomGenerator generator = new RandomGenerator();
 
-generator.generateInputData(10); // Dimensiunea setului de date
+//generator.generateInputData(10); // Dimensiunea setului de date
 
 
 
 // Calea absolută către fișierul de intrare
+string filePath = @"D:\Data\Work\Proiect MEP\MEPSortingAlgorithms\MEPSortingAlgorithms\input\data10000.txt";
 
-//string filePath = @"D:\Data\Work\Proiect MEP\MEPSortingAlgorithms\MEPSortingAlgorithms\input\data10000.txt";
-
-//BubbleSort.RunBubbleSort(filePath);
+BubbleSort.RunBubbleSort(filePath);
--- seqvential/BubbleSort.cs
+++ seqvential/BubbleSort.cs
@@ -77,7 +77,9 @@ namespace MEPSortingAlgorithms.seqvential
         {
             int n = arr.Length;
             for (int i = 0; i < n - 1; i++)
+            {
                 for (int j = 0; j < n - i - 1; j++)
+                {
                     if (arr[j] > arr[j + 1])
                     {
                         // swap temp and arr[i]
@@ -85,6 +87,8 @@ namespace MEPSortingAlgorithms.seqvential
                         arr[j] = arr[j + 1];
                         arr[j + 1] = temp;
                     }
+                }
+            }
         }
     }
 }
