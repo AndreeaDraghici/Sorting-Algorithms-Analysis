649fcba451732515ff7d047c6cfea6da5318742d
AndreeaDraghici <draghici108@gmail.com>
2024-03-02 20:01:13 +0200

    implemented secvential selection sort algorithm

--- implementation/seqvential/SelectionSort.cs
+++ implementation/seqvential/SelectionSort.cs
@@ -1,6 +1,66 @@
-﻿namespace MEPSortingAlgorithms.algorith.seqvential
+﻿using MEPSortingAlgorithms.utils;
+using System.Diagnostics;
+
+namespace MEPSortingAlgorithms.algorith.seqvential
 {
     public class SelectionSort
     {
+        private readonly ISortHelper sortHelper;
+
+        public SelectionSort(ISortHelper sortHelper)
+        {
+            this.sortHelper = sortHelper;
+        }
+
+        public void RunSelectionSort(string inputFilePath)
+        {
+            ArgumentNullException.ThrowIfNull(inputFilePath);
+
+            int[] arr = sortHelper.ReadDataFromFile(inputFilePath);
+
+            if (arr is null)
+            {
+                Console.WriteLine("Input file is empty or contains invalid data.");
+                return;
+            }
+
+            Console.WriteLine("Array before sorting:");
+            sortHelper.PrintArray(arr);
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            SelectionSortAlgorithm(arr);
+            stopwatch.Stop();
+
+            Console.WriteLine("\nArray after sorting:");
+            sortHelper.PrintArray(arr);
+            Console.WriteLine($"\nTime taken to sort: {stopwatch.Elapsed.TotalSeconds * 10} seconds");
+        }
+
+        private void SelectionSortAlgorithm(int[] arr)
+        {
+            int size = arr.Length;
+
+            // Loop through the array elements.
+            for (int i = 0; i < size - 1; i++)
+            {
+                // Assume the minimum is the first element of the unsorted part.
+                int minIndex = i;
+
+                // Search the rest of the array to find the actual minimum.
+                for (int idx = i + 1; idx < size; idx++)
+                {
+                    // If a smaller element is found, update minIndex with its index.
+                    if (arr[idx] < arr[minIndex])
+                    {
+                        minIndex = idx;
+                    }
+                }
+
+                // Swap the found minimum element with the first element of the unsorted part.
+                // This effectively grows the sorted part of the array by one element.
+                (arr[minIndex], arr[i]) = (arr[i], arr[minIndex]);
+            }
+        }
     }
 }
