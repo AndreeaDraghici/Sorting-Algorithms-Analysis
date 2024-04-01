b813d0cc45d54914eb2bc30081c8f421de13f3f8
AndreeaDraghici <draghici108@gmail.com>
2024-04-01 22:46:50 +0300

    analysis results added and fuux bugs on execution time computing

--- implementation/seqvential/QuickSortSecvential.cs
+++ implementation/seqvential/QuickSortSecvential.cs
@@ -35,7 +35,7 @@ namespace MEPSortingAlgorithms.algorith.seqvential
 
             sortHelper.PrintArray(arr, "Array after sorting: ");
 
-            double executionTime = stopwatch.Elapsed.TotalSeconds * 10;
+            double executionTime = stopwatch.Elapsed.TotalSeconds;
             Console.WriteLine($"\nTime taken to sort: {executionTime} seconds");
 
             return executionTime;
