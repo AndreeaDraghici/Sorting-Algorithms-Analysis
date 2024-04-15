f2310e88b0c47a1eb2a0fd38368fad6477cff8b1
AndreeaDraghici <draghici108@gmail.com>
2024-04-15 12:58:38 +0300

    generated new data set and analyze the output results

--- Program.cs
+++ Program.cs
@@ -1,39 +1,23 @@
-﻿using MEPSortingAlgorithms.implementation;
-using MEPSortingAlgorithms.utils;
+﻿using MEPSortingAlgorithms.utils;
+using MEPSortingAlgorithms.algorith;
 using MEPSortingAlgorithms.utils.iface;
-using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Running;
+using MEPSortingAlgorithms.implementation;
 
 namespace MEPSortingAlgorithms
 {
     public class Program
     {
 
+        /*  public static void Main(string[] args)
+          {
+              RandomGenerator generator = new RandomGenerator();
+              Constants constants = new Constants();
+              generator.generateInputDataSet(100000,100000, constants.filePath);
 
-        public static void Main(string[] args)
-        {
-            try
-            {
-                // Initialization
-                ISortHelper sortHelper = new SortHelper();
-                IExecutionTimeManager executionTimeManager = new ExecutionTimeManager();
-                Constants constants = new Constants();
+          }
+  */
 
-                // Sequential sorting execution
-                SeqventialSortingManager sequentialSorting = new SeqventialSortingManager(sortHelper, constants.filePath, executionTimeManager);
-                sequentialSorting.RunAndExportSortingAlgorithms(constants.secventialOutputPath);
-                Console.WriteLine("Sequential sorting algorithms executed and results exported successfully.");
-
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"An error occurred during the execution: {ex.Message}");
-            }
-        }
-
-
-
-        /* public static async Task Main(string[] args)
+        /* public static void Main(string[] args)
          {
              try
              {
@@ -42,16 +26,38 @@ namespace MEPSortingAlgorithms
                  IExecutionTimeManager executionTimeManager = new ExecutionTimeManager();
                  Constants constants = new Constants();
 
-                 // Parallel sorting execution
-                 ParallelSortingManager parallelSorting = new ParallelSortingManager(sortHelper, constants.filePath, executionTimeManager);
-                 await parallelSorting.RunAndExportSortingAlgorithmsAsync(constants.parallelOutputPath);
-                 Console.WriteLine("\nParallel sorting algorithms executed and results exported successfully.");
+                 // Sequential sorting execution
+                 SeqventialSortingManager sequentialSorting = new SeqventialSortingManager(sortHelper, constants.filePath, executionTimeManager);
+                 sequentialSorting.RunAndExportSortingAlgorithms(constants.secventialOutputPath);
+                 Console.WriteLine("Sequential sorting algorithms executed and results exported successfully.");
+
              }
              catch (Exception ex)
              {
                  Console.WriteLine($"An error occurred during the execution: {ex.Message}");
              }
-
          }*/
+
+
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                // Initialization
+                ISortHelper sortHelper = new SortHelper();
+                IExecutionTimeManager executionTimeManager = new ExecutionTimeManager();
+                Constants constants = new Constants();
+
+                // Parallel sorting execution
+                ParallelSortingManager parallelSorting = new ParallelSortingManager(sortHelper, constants.filePath, executionTimeManager);
+                await parallelSorting.RunAndExportSortingAlgorithmsAsync(constants.parallelOutputPath);
+                Console.WriteLine("\nParallel sorting algorithms executed and results exported successfully.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred during the execution: {ex.Message}");
+            }
+
+        }
     }
 }
